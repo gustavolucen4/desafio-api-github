b1700e6cee030a4f66e8983bbefe47b56b881efe
Gustavo Lucena <gustavolucena12@gmail.com>
2023-03-09 17:02:13 -0300

    Melhorando projeto

--- src/Service/user-service.ts
+++ src/Service/user-service.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const apiUrl = 'https://api.github.com';
+import { GITHUB_API_URL } from '../utils/system';
 
 // em caso de 403 por muitas requisições
 //const accessToken = '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';
@@ -13,5 +12,5 @@ const apiUrl = 'https://api.github.com';
 })*/
 
 export function findByLogin(login: string){
-    return axios.get(`https://api.github.com/users/${login}`);
+    return axios.get(`${GITHUB_API_URL}/users/${login}`);
 }
